24583f72f86058110883109faa09acb10d16c4a3
justdilah <nurdilah352@gmail.com>
2023-12-05 23:48:18 +0800

    KNN implementation

---
+++ model_knn.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix, roc_curve,auc
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+my_data_forged = pd.read_csv("late_fusion_fake_1.csv")
+my_data_forged = my_data_forged[:10000]
+columns = ['left_eye_classifier', 'right_eye_classifier', 'nose_classfier', 'mouth_classifier']
+
+# columns = ['corr_max_1', 'corr_max_2', 'corr_max_3', 'corr_max_4','corr_max_5',
+#             'corr_max_6','corr_max_7', 'eudist_1', 'eudist_2', 'eudist_3', 'eudist_4', 'eudist_5',
+#            'eudist_6', 'eudist_7', 'eudist_8', 'Variance', 'Entropy']
+
+X_forged = my_data_forged[columns]
+y_forged = my_data_forged['label']
+
+my_data_original = pd.read_csv("late_fusion_real_1.csv")
+my_data_original = my_data_original[:10000]
+X_original = my_data_original[columns]
+y_original = my_data_original['label']
+
+
+X = pd.concat([X_forged, X_original], axis=0)
+y = pd.concat([y_forged, y_original], axis=0)
+# training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+# standardize the range of values
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+# Creating KNN
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+# Evaluate the performance
+knn_accuracy = accuracy_score(y_test, y_pred)
+knn_precision = precision_score(y_test, y_pred, pos_label='fake')
+knn_recall = recall_score(y_test, y_pred, pos_label='fake')
+knn_f1 = f1_score(y_test, y_pred, pos_label='fake')
+print("KNN Accuracy:", knn_accuracy)
+print("KNN Precision:", knn_precision)
+print("KNN Recall:", knn_recall)
+print("KNN F1 Score:", knn_f1)
